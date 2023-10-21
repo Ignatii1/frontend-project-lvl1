d9105836dbc3757e35b1d24537615716c2149fe8
Ignatii1 <qinfen@ya.ru>
2023-10-21 19:29:03 +0400

    calc js refactor

--- games/calc.js
+++ games/calc.js
@@ -1,30 +1,29 @@
 import readlineSync from 'readline-sync';
 import getRandomNumber from '../src/randomNumber.js';
 
-
 function gameCalc() {
-    const firstNumber = getRandomNumber(100);
-    const secondNumber = getRandomNumber(100);
-    const operators = ['*', '+', '-'];
-    const randomOperator = operators[Math.floor(Math.random() * operators.length)]
-    let userAnswer = readlineSync.question(`What is the result of the expression? ${firstNumber} ${randomOperator} ${secondNumber} `);
-    let correctAnswer;
-    switch (randomOperator) {
-        case '*':
-            correctAnswer = firstNumber * secondNumber;
-            break;
-        case '-':
-            correctAnswer = firstNumber - secondNumber;
-            break;
-        case '+':
-            correctAnswer = firstNumber + secondNumber;
-            break;
-        default:
-            console.log('Invalid operator');
-            process.exit();
-    }
-    userAnswer = parseInt(userAnswer, 10);
-    return { userAnswer, correctAnswer };
+  const firstNumber = getRandomNumber(100);
+  const secondNumber = getRandomNumber(100);
+  const operators = ['*', '+', '-'];
+  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+  let userAnswer = readlineSync.question(`What is the result of the expression? ${firstNumber} ${randomOperator} ${secondNumber} `);
+  let correctAnswer;
+  switch (randomOperator) {
+    case '*':
+      correctAnswer = firstNumber * secondNumber;
+      break;
+    case '-':
+      correctAnswer = firstNumber - secondNumber;
+      break;
+    case '+':
+      correctAnswer = firstNumber + secondNumber;
+      break;
+    default:
+      console.log('Invalid operator');
+      process.exit();
+  }
+  userAnswer = parseInt(userAnswer, 10);
+  return { userAnswer, correctAnswer };
 }
 
 export default gameCalc;
